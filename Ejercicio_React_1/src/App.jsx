d46c3be519978446284f85fc54b7643b31977959
WilmerProfesor <wilmerprofesor@gmail.com>
2025-03-27 09:06:48 -0500

    Exercise React

---
+++ Ejercicio_React_1/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Header from "./Components/Header/Header";
+import CardTest from "./Components/CardTest/CardTest";
+
+
+import "./App.css"
+
+const App = () => {
+
+const arrayObjects = [
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
+]
+
+  return (
+    <>
+      <Header />
+      {arrayObjects.map((item)=>{
+        return (
+          <CardTest img={item.img} />
+        )
+      })}      
+    </>
+  )
+}
+
+export default App
---
+++ Ejercicio_React_1/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Test Image</h1>
+        <div id='header-counter'>
+            X img
+        </div>
+        <p>Testing image for React</p>
+    </header>
+  )
+}
+
+export default Header
