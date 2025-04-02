4aaa4a94013ef595e6cca2938d0be3b182869cfd
WilmerProfesor <wilmerprofesor@gmail.com>
2025-04-02 08:15:53 -0500

    Update exercise

--- Ejercicio_React_1/src/App.jsx
+++ Ejercicio_React_1/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import Header from "./Components/Header/Header";
 import CardTest from "./Components/CardTest/CardTest";
@@ -8,7 +8,14 @@ import "./App.css"
 
 const App = () => {
 
+  
+
 const arrayObjects = [
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
+  {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
   {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
   {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
   {img:"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"},
@@ -18,11 +25,14 @@ const arrayObjects = [
   return (
     <>
       <Header />
-      {arrayObjects.map((item)=>{
-        return (
-          <CardTest img={item.img} />
-        )
-      })}      
+      <main>
+        {arrayObjects.map((item)=>{
+          return (
+            <CardTest img={item.img} />
+          )
+        })}      
+
+      </main>
     </>
   )
 }
--- Ejercicio_React_1/src/Components/Header/Header.jsx
+++ Ejercicio_React_1/src/Components/Header/Header.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import "./Header.css";
 
 const Header = () => {
+
+  const [counter, setCounter] = useState(0);
+  
+  useEffect(()=>{
+    console.log("Contador Actualizado"+counter);        
+    // setCounter(counter+1); si activa esta línea el useEffect se dispara infinitamente
+  })
+
+  useEffect(()=>{
+    console.log("Se cargó la página"+counter);          
+  },[]) // este useEffect se dispara al cargar por primera vez la pág
+  
+  useEffect(()=>{
+    console.log("Algo pasó con el estado");          
+  },[counter]) // este useEffect se dispara al cargar por primera vez la pág
+
+
+  const handleIncrement=()=>{
+    setCounter(counter+1);    
+  }
+
+  const handleDecrement=()=>{
+    setCounter(counter-1);    
+  }
+
   return (
     <header>
         <h1>Test Image</h1>
         <div id='header-counter'>
-            X img
+            {counter} img
         </div>
+        <button onClick={handleIncrement}>Incrementar</button>
+        <button onClick={handleDecrement}>Decrementar</button>
         <p>Testing image for React</p>
     </header>
   )
