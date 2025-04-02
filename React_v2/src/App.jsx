ae589f53692c4f096d2405115916c5fe803d26bc
WilmerProfesor <wilmerprofesor@gmail.com>
2025-04-02 07:19:24 -0500

    react v2

--- React_v2/src/App.jsx
+++ React_v2/src/App.jsx
@@ -4,22 +4,63 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header/Header'
 import CardPersonal from './Components/CardPersonal/CardPersonal'
+import AppleIcon from '@mui/icons-material/Apple';
 
-const App=()=> {
+const App = () => {
 
-  const PI=3.1416;
+  const PI = 3.1416;
+  const info = [
+    {
+      img: "https://estaticos.elcolombiano.com/binrepository/846x565/33c0/780d565/none/11101/UJXN/dambin-98_46143042_20240925181621.jpg",
+      name: "uno",
+      text_img: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque."
+    },
+    {
+      img: "https://lh6.googleusercontent.com/proxy/4u-L8sKA0PWGDkmWGAZ58pnIN0evmTyegc-34NGKw5zRWH2-EjvYfPPm-cwFGW21q4bkwZomzfFz5TTJpiCxZP232GMFwLXMZtkrDRSbesiFDylzoqdXfRQrr31iKYL30w",
+      name: "dos",
+      text_img: "xxxxx xxxxxx xxxxxx xxxxxx xxxxx."
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9QAMTdzf8lbM8G2--17eVTeMMYSvPAUt3A&s",
+      name: "tres",
+      text_img: "Yyyyy yyy yyyyyy yyyyyyy yyyyy."
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9QAMTdzf8lbM8G2--17eVTeMMYSvPAUt3A&s",
+      name: "cuatro",
+      text_img: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque."
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9QAMTdzf8lbM8G2--17eVTeMMYSvPAUt3A&s",
+      name: "cinco",
+      text_img: "xxxxx xxxxxx xxxxxx xxxxxx xxxxx."
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9QAMTdzf8lbM8G2--17eVTeMMYSvPAUt3A&s",
+      name: "seis",
+      text_img: "xzsdsdfwfg regtty rty rty rtutyu tru utyrty ."
+    },
+  ];
 
-  return(
-    <>    
-      <Header/>
+  return (
+    <>
+      <Header />
       <div className='container'>
-        <h1>Hola mundo {PI}</h1>
-        <p style={{marginBottom:"20px"}}>Esta es mi App v 0.0.001</p>  
+      <AppleIcon 
+        sx={{fontSize:"5rem"}}
+      />
+      <h1>Hola mundo {PI}</h1>
+        <p style={{ marginBottom: "20px" }}>Esta es mi App v 0.0.001</p>
       </div>
       <div id='App-container-cards'>
-        <CardPersonal image={"https://estaticos.elcolombiano.com/binrepository/846x565/33c0/780d565/none/11101/UJXN/dambin-98_46143042_20240925181621.jpg"} name={"uno"}/>
-        <CardPersonal name={"dos"}/>
-        <CardPersonal name={"tres"}/>
+        {info.map(
+          (pedro) => {
+            return (
+              <CardPersonal image={pedro.img} name={pedro.name} text={pedro.text_img} />
+            )
+  
+          }          
+        )}        
       </div>
     </>
   )
--- React_v2/src/Components/CardPersonal/CardPersonal.jsx
+++ React_v2/src/Components/CardPersonal/CardPersonal.jsx
@@ -8,24 +8,25 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardActions from '@mui/material/CardActions';
 import "./CardPersonal.css";
 
-const CardPersonal=({name, image})=> {
+
+const CardPersonal=({name, image, text})=> {
   return (
     <div id='Card-container'>
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia
             component="img"
-            height="140"
+            height="140"            
             image={image==undefined?"https://zooloexoticos.wordpress.com/wp-content/uploads/2017/05/iguana-2039719_960_720.jpg?w=640":image}
             alt="green iguana"
+            sx={{maxWidth: 340}}  //con este código modifico el css del componente
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               {name}
             </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica
+              {text}
             </Typography>
           </CardContent>
         </CardActionArea>
